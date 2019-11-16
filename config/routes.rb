77221c565bc0408610f3a4e6cec6dc9dfd6ef866
xJoDenx <weider.in.jelly.61@gmail.com>
2019-11-16 18:43:11 +0900

    create_calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'events#index'
+  resources :events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
