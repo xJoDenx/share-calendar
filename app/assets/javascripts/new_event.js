3025f7481427f0ae5cb55e199ea69c72e8a95f4c
xJoDenx <weider.in.jelly.61@gmail.com>
2019-12-15 20:32:44 +0900

    edit_new_eventimage

--- app/assets/javascripts/new_event.js
+++ app/assets/javascripts/new_event.js
@@ -28,4 +28,64 @@ $(document).on('turbolinks:load', function() {
   $('.form-glid').on('click', '.remove-cast-btn', function(){
     $(this).parent().remove()
   });
+
+
+  // イメージのjs
+  function addImgFormHTML(){
+    // 生成する文字列の長さ
+    var l = 8;
+    // 生成する文字列に含める文字セット
+    var c = "0123456789";
+
+    var cl = c.length;
+    var r = "";
+    for(var i=0; i<l; i++){
+      r += c[Math.floor(Math.random()*cl)];
+    }
+    var html = `<div class= 'new-event__field__img'>
+                  <label class= "img-field" for= "event-form-${r}">
+                    <div class= "img-field__contents">
+                      <i class= "fa fa-picture-o img-field--icon"></i>
+                      <div class= "img-field__contents--cr">
+                        画像を選択
+                      </div>
+                    </div>
+                    <input id= "event-form-${r}" class= "event-form__img" type= "file" name="event[event_images_attributes][${r}][img]">
+                  </label>
+                  <div class= "remove-img-btn">
+                    <i class="fa fa-times-circle-o"></i>
+                  </div>
+                </div>`
+    return html;
+  }
+
+  function addImgPrevHTML(img){
+    var html = `<img class= 'ev-new-img-prev' src= '${img}'>`
+    return html;
+  }
+  $('.add-img-btn').on('click', function(){
+    $('.ev-new-img-glid').append(addImgFormHTML)
+    if ($('.new-event__field__img').length == 4){
+      $('.add-img-btn').hide();
+    }
+  });
+
+  $('.ev-new-img-glid').on('change', '.event-form__img', function(e) {
+    $(this).parent().hide();
+    var parent = $(this).parent().parent()
+    var file = $(this).prop('files')[0];
+    var reader = new FileReader()
+    reader.onload = (function(e) {
+        var img = e.target.result
+        parent.prepend(addImgPrevHTML(img))
+    });
+    reader.readAsDataURL(file);
+  });
+
+  $('.ev-new-img-glid').on('click', '.remove-img-btn', function(){
+    $(this).parent().remove()
+    if ($('.new-event__field__img').length != 4){
+      $('.add-img-btn').show();
+    }
+  });
 });
