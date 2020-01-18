eae680f9fce09f4f4755ca7c576ee90bd6f5ebad
xJoDenx <weider.in.jelly.61@gmail.com>
2020-01-18 22:12:48 +0900

    add_follow

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :events
   resources :users, only: [:show, :edit, :update]
   resources :tweets, only: [:index, :new, :create]
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
